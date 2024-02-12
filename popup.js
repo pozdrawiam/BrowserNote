1069642f7ee1538d4508f78d9a6393b9f73449c2
David <witam@tuta.io>
2024-02-12 21:03:47 +0100

    Badge with not empty line numbers

--- popup.js
+++ popup.js
@@ -3,9 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     chrome.storage.local.get(['note'], function (result) {
         noteElement.value = result.note || '';
+        updateBadge(result.note);
     });
 
     noteElement.addEventListener('input', () => {
         chrome.storage.local.set({ note: noteElement.value });
+        updateBadge(noteElement.value);
     });
 });
+
+function updateBadge(note) {
+    const nonEmptyLinesCount = note.split('\n').filter(line => line.trim() !== '').length;
+    const badgeText = nonEmptyLinesCount > 0 ? nonEmptyLinesCount.toString() : '';
+
+    chrome.action.setBadgeText({ text: badgeText });
+}
