c273b31beab80f228d4f7f0dc516790de0e64696
David <witam@tuta.io>
2024-02-15 20:58:37 +0100

    Fix load badge when no note

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,10 +1,18 @@
 export function loadBadge() {
     chrome.storage.local.get(['note'], function (result) {
+        if (!result) {
+            return;
+        }
+
         updateBadge(result.note);
     });
 }
 
 export function updateBadge(note) {
+    if (!note) {
+        return;
+    }
+
     const nonEmptyLinesCount = note.split('\n').filter(line => line.trim() !== '').length;
     const badgeText = nonEmptyLinesCount > 0 ? nonEmptyLinesCount.toString() : '';
 
