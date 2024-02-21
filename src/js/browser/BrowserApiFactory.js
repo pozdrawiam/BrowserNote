1f0cea9abc3af04e128853dd381c34387e40b0ca
David <witam@tuta.io>
2024-02-21 19:40:08 +0100

    Code reformat

--- src/js/browser/BrowserApiFactory.js
+++ src/js/browser/BrowserApiFactory.js
@@ -5,7 +5,7 @@ export default class BrowserApiFactory {
     static create() {
         if (typeof chrome !== "undefined" && chrome.action) {
             return new ChromeBrowserApi();
-        } 
+        }
         else if (typeof browser !== "undefined" && browser.action) {
             return new FirefoxBrowserApi();
         }
